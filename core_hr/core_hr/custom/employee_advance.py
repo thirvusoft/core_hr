b49c4d1fb87990c76d7980834ac1e9fba891b33c
MOHAMMEDSAHEETH <saheeth@thirvusoft.in>
2022-06-17 16:39:17 +0530

    Automatically create payment entry after submitting employee advance and show if employee have any pending advance

--- core_hr/core_hr/custom/employee_advance.py
+++ core_hr/core_hr/custom/employee_advance.py
@@ -1,20 +1,20 @@
 import frappe
 def create_payment_entry(doc,action):
-        add_doc=frappe.new_doc('Payment Entry')
-        add_doc.payment_type='Pay'
-        add_doc.party_type = 'Employee'
-        add_doc.party = doc.employee
-        add_doc.posting_date = doc.posting_date
-        add_doc.posting_date = doc.posting_date
-        add_doc.paid_from_account_currency = doc.currency
-        add_doc.mode_of_payment = doc.mode_of_payment
-        add_doc.paid_amount=doc.advance_amount
-        add_doc.received_amount=doc.advance_amount
-        add_doc.source_exchange_rate=1.0
-        add_doc.target_exchange_rate=1.0
-        add_doc.paid_to=doc.advance_account
-        add_doc.paid_from=doc.advance_account
-        add_doc.append(
+        payment_doc=frappe.new_doc('Payment Entry')
+        payment_doc.payment_type='Pay'
+        payment_doc.party_type = 'Employee'
+        payment_doc.party = doc.employee
+        payment_doc.posting_date = doc.posting_date
+        payment_doc.posting_date = doc.posting_date
+        payment_doc.paid_from_account_currency = doc.currency
+        payment_doc.mode_of_payment = doc.mode_of_payment
+        payment_doc.paid_amount=doc.advance_amount
+        payment_doc.received_amount=doc.advance_amount
+        payment_doc.source_exchange_rate=1.0
+        payment_doc.target_exchange_rate=1.0
+        payment_doc.paid_to=doc.advance_account
+        payment_doc.paid_from=doc.advance_account
+        payment_doc.append(
 		"references",
 		{
                         "reference_doctype": 'Employee Advance',
@@ -25,6 +25,6 @@ def create_payment_entry(doc,action):
                 },
         )
 
-        add_doc.insert()
-        add_doc.submit()        
+        payment_doc.insert()
+        payment_doc.submit()        
         frappe.db.commit()
