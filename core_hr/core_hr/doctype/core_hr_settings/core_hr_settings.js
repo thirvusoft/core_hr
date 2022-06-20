1c7deb1fea8c38d50626cbc57ac9ba6eb3c4a231
Shankarv19bcr <shankar@thirvusoft.in>
2022-06-20 12:10:19 +0530

    feat:core hr settings doctype created

---
+++ core_hr/core_hr/doctype/core_hr_settings/core_hr_settings.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2022, TS and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Core HR Settings', {
+	// refresh: function(frm) {
+
+	// }
+});
---
+++ core_hr/core_hr/doctype/core_hr_settings/test_core_hr_settings.py
@@ -0,0 +1,8 @@
+# Copyright (c) 2022, TS and Contributors
+# See license.txt
+
+# import frappe
+import unittest
+
+class TestCoreHRSettings(unittest.TestCase):
+	pass
