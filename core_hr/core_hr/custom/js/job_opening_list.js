3a2d5f61af6a4a5ae1d92dd52cb173f6d7de2ee7
ChandanaSPthirvusoft <chandana@thirvusoft.in>
2022-06-23 16:56:57 +0530

    feat:checkout deleted

--- core_hr/core_hr/custom/js/job_opening_list.js
+++ core_hr/core_hr/custom/js/job_opening_list.js
@@ -1,13 +1,10 @@
-frappe.db.get_single_value("Core HR Settings","ts_job_opening_indicator").then(value =>{
-    if(value==1){
-		frappe.listview_settings['Job Opening'] = {
-			get_indicator:function(doc){
-				if(doc.status==="Open") {
-					return [("Open"), "green", "status,=,Open"];
-				} else if(doc.status==="Closed") {
-					return [("Closed"), "red", "status,=,Closed"];
-				}
-			}
-		};
+
+frappe.listview_settings['Job Opening'] = {
+	get_indicator:function(doc){
+		if(doc.status==="Open") {
+			return [("Open"), "green", "status,=,Open"];
+		} else if(doc.status==="Closed") {
+			return [("Closed"), "red", "status,=,Closed"];
+		}
 	}
-})
+};
