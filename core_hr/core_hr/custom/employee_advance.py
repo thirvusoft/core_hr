d424e3d11c5ffa9a544b14ced0268cf8c5f696a4
NaviN <navin@thirvusoft.in>
2022-09-27 12:38:06 +0530

    chores:Employee Advance fix

--- core_hr/core_hr/custom/employee_advance.py
+++ core_hr/core_hr/custom/employee_advance.py
@@ -3,13 +3,11 @@ from erpnext.hr.doctype.employee_advance.employee_advance import make_bank_entry
 def create_payment_entry(doc,action):
     ts_employee_advance_automation = frappe.db.get_single_value("Core HR Settings", "ts_employee_advance_automation")
     if ts_employee_advance_automation==1:
-                
                 payment_doc = make_bank_entry(doc.doctype,doc.name)
                 pd = frappe.new_doc('Journal Entry')
-                # company = frappe.get_doc('Company',doc.company)
-                # abbr=company.abbr
-                # payment_doc.posting_date=doc.posting_date
-                # payment_doc.paid_from = doc.advance_account
                 pd.update(payment_doc)
+                for row in pd.accounts:
+                    row.location=doc.location
+                pd.voucher_type="Journal Entry"
                 pd.insert()
-                # payment_doc.submit()
+                pd.submit()
