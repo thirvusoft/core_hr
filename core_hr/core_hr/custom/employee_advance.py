067160512046c65389b535d08d5d3a003a885115
ChandanaSPthirvusoft <chandana@thirvsoft.in>
2022-09-27 04:39:02 +0000

    live_site_changes

--- core_hr/core_hr/custom/employee_advance.py
+++ core_hr/core_hr/custom/employee_advance.py
@@ -1,12 +1,15 @@
 import frappe
-from erpnext.accounts.doctype.payment_entry.payment_entry import get_payment_entry
+from erpnext.hr.doctype.employee_advance.employee_advance import make_bank_entry
 def create_payment_entry(doc,action):
     ts_employee_advance_automation = frappe.db.get_single_value("Core HR Settings", "ts_employee_advance_automation")
     if ts_employee_advance_automation==1:
-                payment_doc = get_payment_entry(doc.doctype,doc.name)
-                company = frappe.get_doc('Company',doc.company)
-                abbr=company.abbr
-                payment_doc.posting_date=doc.posting_date
-                payment_doc.paid_from = 'Cash - '+abbr
-                payment_doc.save()
-                payment_doc.submit()
+                
+                payment_doc = make_bank_entry(doc.doctype,doc.name)
+                pd = frappe.new_doc('Journal Entry')
+                # company = frappe.get_doc('Company',doc.company)
+                # abbr=company.abbr
+                # payment_doc.posting_date=doc.posting_date
+                # payment_doc.paid_from = doc.advance_account
+                pd.update(payment_doc)
+                pd.insert()
+                # payment_doc.submit()
